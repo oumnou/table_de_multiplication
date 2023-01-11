5fca2c0a10951ec613795afc70487b38ce39f697
Oumaima Nouni <oumaimanouni08@gmail.com>
2023-01-11 13:38:53 +0100

    App created

---
+++ app/src/main/java/com/example/myapplicationwithgit/TableDeMultiplication.java
@@ -0,0 +1,13 @@
+package com.example.myapplicationwithgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableDeMultiplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
