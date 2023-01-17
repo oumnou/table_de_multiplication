177af680ef61a46d25c955297991dc6a075dfb2b
Oumaima Nouni <oumaimanouni08@gmail.com>
2023-01-17 14:07:25 +0100

    Multiplication : done

--- app/src/main/java/com/example/myapplicationwithgit/TableDeMultiplication.java
+++ app/src/main/java/com/example/myapplicationwithgit/TableDeMultiplication.java
@@ -1,17 +1,22 @@
 package com.example.myapplicationwithgit;
 
-import static java.lang.System.exit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class TableDeMultiplication extends AppCompatActivity {
 
     EditText edt_num;
     Button btn_reinit, btn_ann, btn_aff;
+    TextView tv_multip;
+    String s = "";
+    int n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +26,34 @@ public class TableDeMultiplication extends AppCompatActivity {
         edt_num = findViewById(R.id.edt_num);
         btn_ann = findViewById(R.id.btn_ann);
         btn_reinit = findViewById(R.id.btn_reinit);
+        tv_multip = findViewById(R.id.tv_multi);
+        btn_aff = findViewById(R.id.btn_aff);
 
-        btn_reinit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                edt_num.setText("");
-            }
+        btn_reinit.setOnClickListener(view -> {
+            edt_num.setText("");
+            tv_multip.setText(R.string.table);
         });
 
-        btn_ann.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.exit(0);
-            }
+        btn_ann.setOnClickListener(view -> System.exit(0));
+
+        btn_aff.setOnClickListener(view -> {
+
+
+
+            String m = edt_num.getText().toString();
+
+
+             n = Integer.parseInt(m);
+
+            for (int i = 0; i < 11; i++) {
+                    s += n + " * " + i + " = " + n*i + "\n" ;}
+
+            tv_multip.setText(s);
+            s = "";
+            edt_num.setText("");
+
         });
+
+
     }
 }
