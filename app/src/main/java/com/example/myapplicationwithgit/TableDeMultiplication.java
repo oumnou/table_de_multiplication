661eb8f72daf589e2b652f78b2742e6900f593eb
Oumaima Nouni <oumaimanouni08@gmail.com>
2023-01-17 14:07:25 +0100

    bug fixes (textView , btn_reinit,label)

--- app/src/main/java/com/example/myapplicationwithgit/TableDeMultiplication.java
+++ app/src/main/java/com/example/myapplicationwithgit/TableDeMultiplication.java
@@ -2,16 +2,19 @@ package com.example.myapplicationwithgit;
 
 import static java.lang.System.exit;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class TableDeMultiplication extends AppCompatActivity {
 
     EditText edt_num;
     Button btn_reinit, btn_ann, btn_aff;
+    TextView tv_multip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +24,15 @@ public class TableDeMultiplication extends AppCompatActivity {
         edt_num = findViewById(R.id.edt_num);
         btn_ann = findViewById(R.id.btn_ann);
         btn_reinit = findViewById(R.id.btn_reinit);
+        tv_multip = findViewById(R.id.tv_multi);
 
         btn_reinit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 edt_num.setText("");
+                tv_multip.setText(R.string.table);
+
+
             }
         });
 
